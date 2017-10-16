3edde26d4efc7373a3e92877aea2175586ffab37
nbrockett <n.brockett@gmail.com>
2017-10-16 10:53:44 +0100

    added handling of multiple connections

--- chat_server.py
+++ chat_server.py
@@ -1,6 +1,7 @@
 # simplest chat server
 
 import socket
+import select
 import sys
 
 HOST = "localhost"
@@ -31,30 +32,81 @@ class ChatServer():
 
         self.port = port
         self.host = host
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clients = {}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.server_socket.setblocking(0)
 
+        #self.clients = {}
+        self.sockets = []
+
+        # bind socket to server using hostname and port
         try:
-            self.server.bind((self.host, self.port))
+            self.server_socket.bind((self.host, self.port))
         except:
             print('Failed to bind socket to server'.format(socket.error))
             sys.exit()
 
-        self.server.listen(n_connections)
+        # listen up to n_connections
+        self.server_socket.listen(n_connections)
+
+        # add open socket to socket list
+        self.sockets.append(self.server_socket)
+
+        print(self.server_socket)
         print("Chat server started on port " + str(PORT))
+
 
     def run(self):
 
         print('Waiting for connections on port'.format(self.port))
 
-        while True:
-            conn, addr = self.server.accept()
-            print("new client connected: {0}, {1}".format(conn, addr))
 
-            data = conn.recv(RECV_BUFFER)
-            print("Received ", repr(data))
+        while True:
+
+            # get list of sockets which are readable, writable or have an exceptional condition
+            # all sockets in readable have data buffered to be read out
+            # all sockets in writable have free space in the buffer to be written to
+            readable_sockets, writable_sockets, exceptional_sockets = select.select(self.sockets, [], [], 0)
+
+            for socket in readable_sockets:
+
+                if socket == self.server_socket:
+                    # new connection
+                    conn, addr = self.server_socket.accept()
+                    self.sockets.append(conn)
+                    print("new client connected: {0}, {1}".format(conn, addr))
+
+                    self.publish_to_all(conn, "[%s:%s] entered our chatting room\n" % addr)
+
+
+                else: # message from client
+
+                    data = socket.recv(RECV_BUFFER)
+                    print("Received ", repr(data))
+
+                    if data:
+                        self.publish_to_all(socket, "\r" + '[' + str(socket.getpeername()) + '] ' + data.decode())
+                    else:
+                        # remove the socket that's broken
+                        if socket in self.sockets:
+                            self.sockets.remove(socket)
+
+                        self.publish_to_all(socket, "Client (%s, %s) is offline\n" % addr)
 
         conn.close()
+
+
+    def publish_to_all(self, socket, message):
+
+        for isocket in self.sockets:
+
+            if isocket != self.server_socket and isocket != socket:
+                try:
+                    isocket.send(message)
+                except:
+                    isocket.close()
+                    if isocket in self.sockets:
+                        self.sockets.remove(isocket)
+
 
     def stop(self):
         pass
