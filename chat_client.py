11f8bf105f0d00f5e8ce4c67d51108f66d555214
nbrockett <n.brockett@gmail.com>
2017-11-24 16:35:03 +0000

    cleanup client code

--- chat_client.py
+++ chat_client.py
@@ -1,12 +1,10 @@
 # simplest chat client
 
 import socket
+import os
 
 HOST = "localhost"
-# HOST = "10.6.55.115"
-# HOST = "10.10.0.204"
 PORT = 8000
-# PORT = 8888
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('Connecting to {0} port {1}'.format(HOST, PORT))
@@ -17,10 +15,9 @@ sock.connect((HOST, PORT))
 HELO_MSG = 'HELO its me\n'
 JOIN_MSG = "JOIN_CHATROOM: {0}\nCLIENT_IP: {1}\nPORT: {2}\nCLIENT_NAME: {3}\n\n"
 LEAVE_MSG = 'LEAVE_CHATROOM: {0}\nJOIN_ID: {1}\nCLIENT_NAME: {2}\n'
-# JOIN_MSG1 = 'JOIN_CHATROOM: {0}\n'
-# JOIN_MSG2 = 'CLIENT_IP: {0}\n'
-# JOIN_MSG3 = 'PORT: {0}\n'
-# JOIN_MSG4 = 'JOIN_ID: {0}\n'
+CHAT_MSG = "CHAT: 1\nJOIN_ID: 1\nCLIENT_NAME: client1\nMESSAGE: My Message\n\n"
+DISCONNECT_MSG = "DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: client1\n\n"
+
 KILL_MSG = 'KILL_SERVICE\n'
 
 try:
@@ -36,45 +33,55 @@ try:
     print("Received ", repr(reply))
     print("\n")
 
-    # # join msg
-    # join_msg = JOIN_MSG.format('chatroom1', '123.456.789.000', '123', 'client1')
-    #
-    # print(join_msg)
-    # print('sending join msg')
-    # sock.send(join_msg.encode('utf-8'))
-    # # sock.send(join_msg2.encode())
-    # # sock.send(join_msg3.encode())
-    # # sock.send(join_msg4.encode())
-    #
-    # print("waiting reply from join")
-    # reply = sock.recv(4096)
-    # print("Received ", repr(reply))
-    # #
-    # # leave msg
-    # leave_msg = LEAVE_MSG.format(1, 1, 'client1')
-    # print('sending leave msg')
-    # sock.send(leave_msg.encode())
-    #
-    # print("waiting reply leave")
-    # reply = sock.recv(4096)
-    # print("Received ", repr(reply))
+    # join msg
+    join_msg = JOIN_MSG.format('chatroom1', '123.456.789.000', '123', 'client1')
+
+    print(join_msg)
+    print('sending join msg')
+    sock.send(join_msg.encode())
+
+    print("waiting reply from join")
+    reply = sock.recv(4096)
+    print("Received ", reply.decode())
+
+    # chat msg
+    chat_msg = CHAT_MSG
+    print(chat_msg)
+    print('sending chat msg')
+    sock.send(chat_msg.encode())
+
+    print("waiting reply from chat")
+    reply = sock.recv(4096)
+    print("Received ", reply.decode())
+
+    # disconnect msg
+    disconnect_msg = DISCONNECT_MSG
+    print(disconnect_msg)
+    print('sending disconnect_msg msg')
+    sock.send(disconnect_msg.encode())
+
+    print("waiting reply from disconnect_msg")
+    reply = sock.recv(4096)
+    print("Received ", reply.decode())
+
+    # leave msg
+    leave_msg = LEAVE_MSG.format(1, 1, 'client1')
+    print('sending leave msg')
+    sock.send(leave_msg.encode())
+
+    print("waiting reply leave")
+    reply = sock.recv(4096)
+    print("Received ", reply.decode())
 
 
+    # kill msg
+    kill_msg = KILL_MSG
+    print('sending kill msg')
+    sock.send(kill_msg.encode())
 
-    # # kill msg
-    # kill_msg = KILL_MSG
-    # print('sending kill msg')
-    # sock.send(kill_msg.encode())
-    #
-    # print("waiting reply kill")
-    # reply = sock.recv(4096)
-    # print("Received ", repr(reply))
 
-    while True:
-        # print("waiting reply from server")
-        # reply = sock.recv(4096)
-        # print("Received ", repr(reply))
-        pass
+except KeyboardInterrupt:
+    os._exit(0)
 
 finally:
     print('Closing socket')
